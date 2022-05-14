ccdce5451cb7382f3101e1fd97e7818a3ff37dee
Asus <charon1996@yandex.ru>
2022-05-14 12:09:31 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Bunin");
+        System.out.println("01.12.1996");
+    }
+}
