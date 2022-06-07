a621d9fdef3be969c6fa9269447693f5fd80efa4
Asus <charon1996@yandex.ru>
2022-06-07 14:39:18 +0300

    4.3.1. Оператор && [#333783 #309912]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -10,9 +10,9 @@ public class Games {
     }
 
     public static void main(String[] args) {
-        Games.permission(true,true);
-        Games.permission(true,false);
-        Games.permission(false,true);
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
         Games.permission(false, false);
     }
 }
